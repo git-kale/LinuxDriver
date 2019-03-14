8ab20ed7155066c5528732f9d875bd02eb50dd6d
git-kale <kjyotiba@cs.iitr.ac.in>
2019-03-14 16:19:49 -0700

    Add a module that prints haow much time it's called

---
+++ chardev/.chardev.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/vip3r/kernel_dev/LinuxDriver/chardev/chardev.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/vip3r/kernel_dev/LinuxDriver/chardev/chardev.ko /home/vip3r/kernel_dev/LinuxDriver/chardev/chardev.o /home/vip3r/kernel_dev/LinuxDriver/chardev/chardev.mod.o ;  true
---
+++ chardev/.tmp_versions/chardev.mod
@@ -0,0 +1,3 @@
+/home/vip3r/kernel_dev/LinuxDriver/chardev/chardev.ko
+/home/vip3r/kernel_dev/LinuxDriver/chardev/chardev.o
+
